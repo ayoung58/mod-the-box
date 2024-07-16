4da7f921ffafde0a32758494aadde8dafe727cc5
Theinterna <yalvin58@gmail.com>
2024-07-15 18:29:47 -0700

    Add random direction change functionality

--- Mod the Box/Assets/ModTheCube/Cube.cs
+++ Mod the Box/Assets/ModTheCube/Cube.cs
@@ -11,6 +11,7 @@ public class Cube : MonoBehaviour
         zAxis
     };
     public Directions direction = Directions.xAxis;
+    public float changeInterval = 3.0f;
     public MeshRenderer Renderer;
     
     void Start()
@@ -21,12 +22,25 @@ public class Cube : MonoBehaviour
         Material material = Renderer.material;
         
         material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+
+        InvokeRepeating("changeDirection", 2.0f, changeInterval);
     }
     
     void Update()
     {   
         // resetDirections();
         rotation();
+    }
+
+    void changeDirection() {
+        int directionCode = Random.Range(0, 3);
+        if (directionCode == 0) {
+            direction = Directions.xAxis;
+        } else if (directionCode == 1) {
+            direction = Directions.yAxis;
+        } else if (directionCode == 2) {
+            direction = Directions.zAxis;
+        }
     }
 
     void rotation() {
